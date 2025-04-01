af9e60e1b21c906b79cd5b8392e280ed5dee4897
ktsotras <kevin.tsotras@gmail.com>
2025-04-01 15:26:10 -0400

    added num chars method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_num_chars
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,5 +7,6 @@ def get_book_text(path_to_file):
 
 def main():
     print(get_num_words(get_book_text("books/frankenstein.txt")))
+    print(get_num_chars(get_book_text("books/frankenstein.txt")))
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,15 @@ def get_num_words(book_text):
     words = book_text.split()
     num_words = len(words)
     return f"{num_words} words found in the document"
+
+def get_num_chars(book_text):
+    count = {}
+
+    for char in book_text:
+        char = char.lower()
+        if char in count:
+            count[char] += 1
+        else:
+            count[char] = 1
+    
+    return count
